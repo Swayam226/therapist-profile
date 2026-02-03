780d031571511d5b53d4de1e87222a63c617c637
Swayam <swayamkumar226@gmail.com>
2026-02-03 17:38:05 +0530

    hero done

--- app/page.js
+++ app/page.js
@@ -4,13 +4,17 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <div className="bg-[#fbf6f1] text-[#223614] w-full h-220 flex flex-row pt-10 overflow-x-hidden items-center font-regular">
+      <div className="bg-[#fbf6f1] text-[#223614] w-full h-220 flex flex-row pt-28 overflow-x-hidden items-center font-regular">
         <div className="relative ml-14 bg-amber-400 h-full w-145 overflow-hidden rounded-t-[999px]">
           <Image src="https://images.squarespace-cdn.com/content/v1/65d10c6adcfabe1819ed4e07/08197a74-c045-4a19-a7c9-0f85c1032bc4/daiga-ellaby-M2P08N9zi2k-unsplash.jpg" priority alt="hero-image" fill className="object-cover"
           />
         </div>
-        <div className="text-[#223614] text-8xl">
-          <span>Live Your Life in Full Bloom</span>
+        <div className="h-full ml-42 pt-48 max-w-xl flex items-center flex-col">
+          <h1 className="text-7xl font-medium leading-tight max-w-md text-center">Live your life in full bloom</h1>
+          <p className="mt-6 text-lg">Therapy for adults in Minneapolis, MN</p>
+          <button className="mt-10 border border-[#223614] px-8 py-3 uppercase tracking-wide bg-transparent transition-colors duration-300 ease-in-out hover:bg-[#223614] hover:text-[#fbf6f1]">
+            Connect with me
+          </button>
         </div>
       </div>
     </main >
--- components/header.js
+++ components/header.js
@@ -1,8 +1,28 @@
+"use client"
 import Link from "next/link"
+import { useState, useEffect } from "react"
 
 export default function Header() {
+    const [hidden, setHidden] = useState(false);
+    const [lastScrollY, setlastScrollY] = useState(0);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const currScrollY = window.scrollY;
+
+            if (currScrollY > lastScrollY && currScrollY > 80) {
+                setHidden(true);
+            } else {
+                setHidden(false);
+            }
+            setlastScrollY(currScrollY);
+        }
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, [lastScrollY])
+
     return (
-        <nav className="w-full overflow-x-hidden h-18 bg-[#fbf6f1] text-[#223614] flex flex-row justify-between items-center">
+        <nav className={`w-full overflow-x-hidden h-18 bg-[#fbf6f1] text-[#223614] flex flex-row justify-between items-center fixed top-0 left-0 z-50 transition-transform duration-500 ease-in-out ${hidden ? "-translate-y-full" : "translate-y-0"}`}>
             <span className="pl-14 text-4xl font-medium"><Link href="/">Lilac Template</Link></span>
             <div className="pr-16 text-xl flex flex-row gap-10">
                 <span><Link href="/blog">Blog</Link></span>
